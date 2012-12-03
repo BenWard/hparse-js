4651f990c63886267ab82f19c069a7e891fc96f6
Ben Ward <ben@benward.me>
2012-12-02 23:54:29 -0800

    Update vocab definition

--- src/vocabularies/hcard.js
+++ src/vocabularies/hcard.js
@@ -1,31 +1,33 @@
 /** hcard for hparse
 Maps the hcard µf1 vocabulary to µf2
 */
-var exports = exports || window;
-
-if (exports.hparse) {
-  exports.hparse.addLegacyVocabulary('h-adr', {
+if (HParse) {
+  HParse.defineLegacyVocabulary('h-adr', {
     'root': ['adr'],
-    'extended-address': 'p-extended-address',
-    'street-address': 'p-street-address',
-    'locality': 'p-locality',
-    'region': 'p-region',
-    'country-name': 'p-country-name',
-    'postal-code': 'p-postal-code'
+    'properties': {
+      'extended-address': 'p-extended-address',
+      'street-address': 'p-street-address',
+      'locality': 'p-locality',
+      'region': 'p-region',
+      'country-name': 'p-country-name',
+      'postal-code': 'p-postal-code'
+    }
   });
 
-  exports.hparse.addLegacyVocabulary('h-card', {
+  HParse.defineLegacyVocabulary('h-card', {
     'root': ['hcard', 'vcard'],
-    'fn': 'p-name',
-    'given-name': 'p-given-name',
-    'additional-name': 'p-additional-name',
-    'family-name': 'p-family-name',
-    'title': 'p-title',
-    'org': 'p-org',
-    'organization-name': 'p-organization-name',
-    'organization-department': 'p-organization-department',
-    'adr': 'h-adr',
-    'geo': 'h-geo',
-    'label': 'h-label'
+    'properties': {
+      'fn': 'p-name',
+      'given-name': 'p-given-name',
+      'additional-name': 'p-additional-name',
+      'family-name': 'p-family-name',
+      'title': 'p-title',
+      'org': 'p-org',
+      'organization-name': 'p-organization-name',
+      'organization-department': 'p-organization-department',
+      'adr': 'p-adr',
+      'geo': 'p-geo',
+      'label': 'p-label'
+    }
   });
 }
